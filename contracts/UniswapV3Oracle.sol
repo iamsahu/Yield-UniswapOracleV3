4dbd42af5e2cea894509f67e1f86cdf9dcf0bd4e
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-09-17 10:35:41 +0100

    tweak: natspec, setSources, variable names

--- contracts/UniswapV3Oracle.sol
+++ contracts/UniswapV3Oracle.sol
@@ -7,17 +7,11 @@ import "@yield-protocol/utils-v2/contracts/cast/CastBytes32Bytes6.sol";
 import "@uniswap/v3-periphery/contracts/libraries/OracleLibrary.sol";
 import "@uniswap/v3-core/contracts/interfaces/pool/IUniswapV3PoolImmutables.sol";
 
-// This for the real deal
-
-// import "../../mocks/oracles/uniswap/UniswapV3OracleLibraryMock.sol";
-
 /**
  * @title UniswapV3Oracle
  */
 contract UniswapV3Oracle is IOracle, AccessControl {
     using CastBytes32Bytes6 for bytes32;
-
-    uint8 public constant decimals = 18;
 
     event SecondsAgoSet(uint32 indexed secondsAgo);
     event SourceSet(
@@ -42,104 +36,15 @@ contract UniswapV3Oracle is IOracle, AccessControl {
     mapping(bytes6 => mapping(bytes6 => Source)) public sources;
     mapping(address => SourceData) public sourcesData;
 
-    /**
-     * @notice Set or reset the number of seconds Uniswap will use for its Time Weighted Average Price computation
-     */
+    /// @dev Set or reset the number of seconds Uniswap will use for its Time Weighted Average Price computation
     function setSecondsAgo(uint32 secondsAgo_) external auth {
         require(secondsAgo_ != 0, "Uniswap must look into the past.");
         secondsAgo = secondsAgo_;
         emit SecondsAgoSet(secondsAgo_);
     }
 
-    /**
-     * @notice Set or reset an oracle source and its inverse
-     */
-    function setSource(
-        bytes6 base,
-        bytes6 quote,
-        address source
-    ) external auth {
-        _setSource(base, quote, source);
-    }
-
-    /**
-     * @notice Set or reset a number of oracle sources
-     */
-    function setSources(
-        bytes6[] memory bases,
-        bytes6[] memory quotes,
-        address[] memory sources_
-    ) external auth {
-        require(
-            bases.length == quotes.length && quotes.length == sources_.length,
-            "Mismatched inputs"
-        );
-        for (uint256 i; i < bases.length; i++) {
-            _setSource(bases[i], quotes[i], sources_[i]);
-        }
-    }
-
-    /**
-     * @notice Retrieve the value of the amount at the latest oracle price.
-     */
-    function peek(
-        bytes32 base,
-        bytes32 quote,
-        uint256 amount
-    )
-        external
-        view
-        virtual
-        override
-        returns (uint256 value, uint256 updateTime)
-    {
-        return _peek(base.b6(), quote.b6(), amount);
-    }
-
-    /**
-     * @notice Retrieve the value of the amount at the latest oracle price. Same as `peek` for this oracle.
-     */
-    function get(
-        bytes32 base,
-        bytes32 quote,
-        uint256 amount
-    ) external virtual override returns (uint256 value, uint256 updateTime) {
-        return _peek(base.b6(), quote.b6(), amount);
-    }
-
-    function _peek(
-        bytes6 base,
-        bytes6 quote,
-        uint256 amount
-    ) private view returns (uint256 value, uint256 updateTime) {
-        Source memory source = sources[base][quote];
-        SourceData memory sourceData;
-        require(source.source != address(0), "Source not found");
-        sourceData = sourcesData[source.source];
-        int24 twapTick = OracleLibrary.consult(source.source, secondsAgo);
-        if (source.inverse) {
-            value = OracleLibrary.getQuoteAtTick(
-                twapTick,
-                (uint128)(amount),
-                sourceData.quoteToken,
-                sourceData.baseToken
-            );
-        } else {
-            value = OracleLibrary.getQuoteAtTick(
-                twapTick,
-                (uint128)(amount),
-                sourceData.baseToken,
-                sourceData.quoteToken
-            );
-        }
-        updateTime = block.timestamp - secondsAgo;
-    }
-
-    function _setSource(
-        bytes6 base,
-        bytes6 quote,
-        address source
-    ) internal {
+    /// @dev Set or reset an oracle source and its inverse
+    function setSource(bytes6 base, bytes6 quote, address source) external auth {
         sources[base][quote] = Source(source, false);
         sources[quote][base] = Source(source, true);
         sourcesData[source] = SourceData(
@@ -150,5 +55,40 @@ contract UniswapV3Oracle is IOracle, AccessControl {
         );
         emit SourceSet(base, quote, source);
         emit SourceSet(quote, base, source);
+    }
+
+    /// @dev Convert amountBase base into quote at the latest oracle price.
+    function peek(bytes32 base, bytes32 quote, uint256 amountBase)
+        external view virtual override
+        returns (uint256 amountQuote, uint256 updateTime)
+    {
+        return _peek(base.b6(), quote.b6(), amountBase);
+    }
+
+    /// @dev Convert amountBase base into quote at the latest oracle price, updating state if necessary. Same as `peek` for this oracle.
+    function get(bytes32 base, bytes32 quote, uint256 amountBase)
+        external virtual override
+        returns (uint256 amountQuote, uint256 updateTime)
+    {
+        return _peek(base.b6(), quote.b6(), amountBase);
+    }
+
+    /// @dev Convert amountBase base into quote at the latest oracle price.
+    function _peek(bytes6 base, bytes6 quote, uint256 amountBase)
+        private view
+        returns (uint256 amountQuote, uint256 updateTime)
+    {
+        Source memory source = sources[base][quote];
+        SourceData memory sourceData;
+        require(source.source != address(0), "Source not found");
+        sourceData = sourcesData[source.source];
+        int24 twapTick = OracleLibrary.consult(source.source, secondsAgo);
+        amountQuote = OracleLibrary.getQuoteAtTick(
+            twapTick,
+            uint128(amountBase),
+            (source.inverse) ? sourceData.quoteToken : sourceData.baseToken,
+            (source.inverse) ? sourceData.baseToken : sourceData.quoteToken
+        );
+        updateTime = block.timestamp - secondsAgo;
     }
 }
